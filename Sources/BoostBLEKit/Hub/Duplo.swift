3a40cbdbe8c592b007d8d5df987fb215c3b32ff9
Shinichiro Oba <ooba@bricklife.com>
2019-01-08 06:12:12 +0900

    Enable to set power

--- Sources/BoostBLEKit/Hub/Duplo.swift
+++ Sources/BoostBLEKit/Hub/Duplo.swift
@@ -14,16 +14,38 @@ public final class Duplo {
         
         public init() {}
         
-        public var connectedIOs: [PortId : IOType] = [:]
+        public var connectedIOs: [PortId : IOType] = [
+            0x00: .trainBaseMotor,
+            0x01: .trainBaseSpeaker,
+            0x11: .rgbLight,
+            0x12: .trainBaseSensor,
+            0x13: .trainBaseSpeedometer,
+            0x14: .voltageSensor,
+            ]
         
-        public let portMap: [Port: PortId] = [:]
+        public let portMap: [Port: PortId] = [
+            .A:     0x00,
+            ]
         
         public func canSupportAsMotor(ioType: IOType) -> Bool {
-            return false
+            switch ioType {
+            case .trainBaseMotor:
+                return true
+            default:
+                return false
+            }
         }
         
         public func motorPowerCommand(port: Port, power: Int8) -> Command? {
-            return nil
+            guard let portId = portId(for: port) else { return nil }
+            guard let ioType = connectedIOs[portId] else { return nil }
+            
+            switch ioType {
+            case .trainBaseMotor:
+                return MotorPowerCommand(portId: portId, power: power)
+            default:
+                return nil
+            }
         }
     }
 }
